adbc95e08df3260d4558879dd2cb93ffb49a46c9
kingfemi1 <speaktokingfemi@gmail.com>
2021-05-18 13:52:05 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var a; //declaration of global variable
+var c;
+var d;
+a = 5; //assignment
+d =10;
+console.log(a); //printing to console
+
+var b = "this is javascript"; //delaration and assignment
+
+console.log(b);
+
+ c = 100;
+ console.log(c)
+
+
+
+ 
+     function add(){
+        var e = 20;//declaration of local variable
+        var f = 30;
+         console.log(a+d);
+ }
+
+
+
+ function sub(){
+     //console.log(e-f);
+ }
+
+ add();
+
+ sub();
